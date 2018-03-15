75e490d415613588952faea17b3b7da4268c284e
Joelhurtado <35708890+Joelhurtado@users.noreply.github.com>
2018-03-15 10:14:22 -0600

    front
    Adecuacion de pantallas

--- src/main/java/mx/appwhere/remittances/front/application/constants/ApplicationConstants.java
+++ src/main/java/mx/appwhere/remittances/front/application/constants/ApplicationConstants.java
@@ -7,5 +7,13 @@ package mx.appwhere.remittances.front.application.constants;
 public final class ApplicationConstants {
     public static final String VIEW_MESSAGE_NAME = "message";
     public static final String VIEWS_PRODUCE_HTML = "text/html; charset=UTF-8";
+    public static final String FORMATO_NUMERO_MUY_LARGO = "Introduce un numero mas corto";
 
+    //Mesnajes de error de validación
+    public static final String NULL_MESSAGE = "No debe estar vacio";
+    public static final String LENGTH_MESSAGE = "El tamaño debe estar entre {min} y {max}";
+    public static final String MIN_MESSAGE = "El valor no debe ser menor a {value}";
+    public static final String MAX_MESSAGE = "El valor no debe de superar a {value}";
+    public static final String DIGITS_MESSAGE = "El valor no debe tener más de {integer} digitos y {fraction} decimales";
+    public static final String SELECT_OPTION_MESSAGE = "Selecciona una opción valida";
 }
